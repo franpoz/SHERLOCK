8c3ac6b012c7bf5b1d26cc83939780229ddd269c
martin.dvpj <martin.devora.pajares@gmail.com>
2020-08-03 21:37:16 +0100

    Adding pip packaging

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sherlock_pipeline-mdevora", # Replace with your own username
+    version="0.0.1",
+    author="Pozuelos",
+    author_email="author@example.com",
+    description="Search for Hints of Exoplanets fRom Lightcurves Of spaCe based seeKers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/franpoz/SHERLOCK",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    packages=find_packages(exclude = ("tests",)),
+    include_package_data=True,
+    install_requires = ["numpy",
+                        "cython",
+                        "pandas",
+                        "lightkurve",
+                        "transitleastsquares",
+                        "requests",
+                        "eleanor",
+                        "wotan",
+                        "matplotlib"
+    ]
+)
