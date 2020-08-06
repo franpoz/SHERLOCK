980df22f9373ffa56684052cd482d0364b132828
martin.dvpj <martin.devora.pajares@gmail.com>
2020-08-06 22:06:34 +0100

    removing repeated key.

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    packages=find_packages(exclude = ("tests",)),
     include_package_data=True,
     install_requires = ["numpy",
                         "cython",
