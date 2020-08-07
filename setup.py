815f9f386ca423c80d6820329013a26a8482b80b
martin.dvpj <martin.devora.pajares@gmail.com>
2020-08-07 13:14:17 +0100

    Updating setup version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sherlockpipe", # Replace with your own username
-    version="0.9.5",
+    version="0.9.6",
     author="F.J. Pozuelos & M. Dévora",
     author_email="fjpozuelos@uliege.be",
     description="Search for Hints of Exoplanets fRom Lightcurves Of spaCe based seeKers",
