804fe639329125c2c83a19eb4b7de78193dc837b
martin.dvpj <martin.devora.pajares@gmail.com>
2020-08-13 15:03:33 +0100

    Adding search zone preparers.

---
+++ sherlockpipe/search_zones/HabitableSearchZone.py
@@ -0,0 +1,19 @@
+import math
+from sherlockpipe.search_zones.SearchZone import SearchZone
+from sherlockpipe.star.HabitabilityCalculator import HabitabilityCalculator
+from sherlockpipe.star.starinfo import StarInfo
+
+
+class HabitableSearchZone(SearchZone):
+    """
+    Implementation for calculating minimum and maximum search periods for the habitable zone of an input star.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def calculate_period_range(self, star_info: StarInfo):
+        hz_aus = HabitabilityCalculator().calculate_hz(star_info.teff, star_info.lum)
+        GM = 7.496e-6 * star_info.mass
+        period_min = math.sqrt((hz_aus[1] ** 3) * GM)
+        period_max = math.sqrt((hz_aus[2] ** 3) * GM)
+        return period_min, period_max
---
+++ sherlockpipe/search_zones/SearchZone.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+from sherlockpipe.star.starinfo import StarInfo
+
+
+class SearchZone(ABC):
+    """
+    Abstract class to be implemented for calculating minimum and maximum search periods for an input star.
+    """
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def calculate_period_range(self, star_info: StarInfo):
+        """
+        Calculates the minimum and maximum periods for the given star_info
+        @param star_info: the star where the range should be calculated
+        @return: a tuple of minimum_period and maximum_period
+        """
+        pass
