ae7f6c0861c91df37992f25a7d6d7add7a02aa91
martin.dvpj <martin.devora.pajares@gmail.com>
2020-10-13 23:11:08 +0100

    fixes out of bounds

--- examples/custom_algorithms/RandomSignalSelector.py
+++ examples/custom_algorithms/RandomSignalSelector.py
@@ -7,7 +7,7 @@ class RandomSignalSelector(SignalSelector):
         super().__init__()
 
     def select(self, transit_results, snr_min, detrend_method, wl):
-        best_signal_snr_index = random.randrange(0, len(transit_results), 1)
+        best_signal_snr_index = random.randrange(0, len(transit_results) - 1, 1)
         best_signal = transit_results[best_signal_snr_index]
         best_signal_snr = best_signal.snr
         if best_signal_snr > snr_min:  # and SDE[a] > SDE_min and FAP[a] < FAP_max):
