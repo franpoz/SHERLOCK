3297f6efe82acc41f1c0817ff195ec3d826c6180
martin.dvpj <martin.devora.pajares@gmail.com>
2020-10-20 10:37:45 +0100

    updated pip installer.

--- pip_installer.sh
+++ pip_installer.sh
@@ -12,8 +12,7 @@ tests_results=$(cat tests.log | grep "FAILED")
 deactivate
 if [[ -z "${tests_results}" ]]; then
   python3 setup.py sdist bdist_wheel
-  pypi=$(cat pypi.txt)
-  python3 -m twine upload dist/* < pypi.txt
+  printf "__token__" | python3 -m twine upload dist/*
 else
   echo "TESTS FAILED. See tests.log"
 fi
