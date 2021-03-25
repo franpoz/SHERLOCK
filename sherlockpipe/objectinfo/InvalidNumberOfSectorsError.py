7946c471c0d6b61faa8d5661d2f1e6d5d69094f6
martin.dvpj <martin.devora.pajares@gmail.com>
2021-03-25 11:22:57 +0000

    removed code moved to lcbuilder

--- sherlockpipe/objectinfo/InvalidNumberOfSectorsError.py
+++
@@ -1,3 +0,0 @@
-class InvalidNumberOfSectorsError(Exception):
-    """Raised when object preparing detects there are more sectors than allowed by setup."""
-    pass
--- sherlockpipe/objectinfo/ObjectProcessingError.py
+++
@@ -1,3 +0,0 @@
-class ObjectProcessingError(Exception):
-    """Raised when object processing fails and wants the other objects to still be processed."""
-    pass
