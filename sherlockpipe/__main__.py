82ea02b52b7cd03d33a4f3a194782fd094623051
martin.dvpj <martin.devora.pajares@gmail.com>
2021-04-17 21:41:28 +0100

    using outliers_sigma.

--- sherlockpipe/__main__.py
+++ sherlockpipe/__main__.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
                                cpu_cores, max_runs, period_min,
                                period_max, period_protect, best_signal_algorithm, quorum_strength,
                                min_quorum, fit_method, oversampling,
-                               t0_fit_margin, duration_grid_step)
+                               t0_fit_margin, duration_grid_step, outliers_sigma)
                 sherlock_targets.append(sherlock_target)
         if mode != "GLOBAL" and mode != "BOTH" and not (mode == "SECTOR" or mode == "BOTH" and isinstance(built_object_info, (
                 MissionObjectInfo, MissionFfiCoordsObjectInfo, MissionFfiIdObjectInfo))):
