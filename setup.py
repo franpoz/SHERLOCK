aca77e6985818550d89bc60de46492cbbe5018db
martin.dvpj <martin.devora.pajares@gmail.com>
2021-04-19 09:50:00 +0100

    updates triceratops version

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setuptools.setup(
                         'configparser==5.0.1',
                         'pyparsing==2.4.7', # Matplotlib dependency
                         'statsmodels==0.12.2', # Allesfitter dependency
-                        'triceratops==1.0.6'
+                        'triceratops==1.0.8'
     ]
 )
