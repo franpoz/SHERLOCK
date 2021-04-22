314038a70916c690e3f507c372f063619dbe31d2
martin.dvpj <martin.devora.pajares@gmail.com>
2021-04-22 11:41:59 +0100

    removed validation from vetting

--- sherlockpipe/vet.py
+++ sherlockpipe/vet.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import yaml
 from astropy.time import Time
+from lcbuilder.lcbuilder_class import LcBuilder
 from lightkurve import TessLightCurve
 from matplotlib.colorbar import Colorbar
 from matplotlib import patches
@@ -206,7 +207,7 @@ class Vetter:
                 traceback.print_exc()
                 # see if it made any plots - often it just fails on the TPs as they are very large
                 if exists("{}/{}/{}_fullLC_md.png".format(indir, tic, tic)):
-                    logging.warn("couldn't download TP but continue anyway")
+                    logging.warning("couldn't download TP but continue anyway")
                     tp_downloaded = False
                     shutil.move(vetter.latte_dir + "/" + tic, transit_results_dir)
                 else:
@@ -287,16 +288,9 @@ class Vetter:
             urls_exist = manifest_table['TICID']
             # get the transit time list
             period = df.loc[df['TICID'] == tic]['period'].iloc[0]
-            duration = df.loc[df['TICID'] == tic]['duration'].iloc[0]
             t0 = df.loc[df['TICID'] == tic]['t0'].iloc[0]
             transit_list = ast.literal_eval(((df.loc[df['TICID'] == tic]['transits']).values)[0])
-            transit_depth = df.loc[df['TICID'] == tic]['depth'].iloc[0]
             candidate_row = candidate.iloc[0]
-            if candidate_row["number"] is None or np.isnan(candidate_row["number"]):
-                lc_file = "/" + candidate_row["lc"]
-            else:
-                lc_file = "/" + str(candidate_row["number"]) + "/lc_" + str(candidate_row["curve"]) + ".csv"
-            lc_file = self.data_dir + lc_file
             try:
                 sectors_in = ast.literal_eval(str(((df.loc[df['TICID'] == tic]['sectors']).values)[0]))
                 if (type(sectors_in) == int) or (type(sectors_in) == float):
@@ -325,15 +319,6 @@ class Vetter:
                 try:
                     sectors_all, ra, dec = LATTEutils.tess_point(indir, tic)
                 except Exception as e1:
-                    traceback.print_exc()
-            if self.validate:
-                try:
-                    result_dir, ra1, dec1 = self.vetting_validation(cpus, indir, tic, sectors, lc_file, transit_depth, period,
-                                                                  t0, duration)
-                    ra = ra if ra is not None else ra1
-                    dec = dec if dec is not None else dec1
-                    shutil.move(result_dir, vetting_dir + "/triceratops")
-                except Exception as e:
                     traceback.print_exc()
             if ra is not None and dec is not None:
                 result_dir = self.vetting_field_of_view(indir, tic, ra, dec, sectors)
@@ -369,157 +354,6 @@ class Vetter:
         #             metadata_data.append(res['starttime'])
         #             writer.writerow(metadata_data)
         # return TIC_wanted
-
-    def vetting_validation(self, cpus, indir, tic, sectors, lc_file, transit_depth, period, t0, transit_duration):
-        """ Calculates probabilities of the signal being caused by any of the following astrophysical sources:
-        TP No unresolved companion. Transiting planet with Porb around target star. (i, Rp)
-        EB No unresolved companion. Eclipsing binary with Porb around target star. (i, qshort)
-        EBx2P No unresolved companion. Eclipsing binary with 2 × Porb around target star. (i, qshort)
-        PTP Unresolved bound companion. Transiting planet with Porb around primary star. (i, Rp, qlong)
-        PEB Unresolved bound companion. Eclipsing binary with Porb around primary star. (i, qshort, qlong)
-        PEBx2P Unresolved bound companion. Eclipsing binary with 2 × Porb around primary star. (i, qshort, qlong)
-        STP Unresolved bound companion. Transiting planet with Porb around secondary star. (i, Rp, qlong)
-        SEB Unresolved bound companion. Eclipsing binary with Porb around secondary star. (i, qshort, qlong)
-        SEBx2P Unresolved bound companion. Eclipsing binary with 2 × Porb around secondary star. (i, qshort, qlong)
-        DTP Unresolved background star. Transiting planet with Porb around target star. (i, Rp, simulated star)
-        DEB Unresolved background star. Eclipsing binary with Porb around target star. (i, qshort, simulated star)
-        DEBx2P Unresolved background star. Eclipsing binary with 2 × Porb around target star. (i, qshort, simulated star)
-        BTP Unresolved background star. Transiting planet with Porb around background star. (i, Rp, simulated star)
-        BEB Unresolved background star. Eclipsing binary with Porb around background star. (i, qshort, simulated star)
-        BEBx2P Unresolved background star. Eclipsing binary with 2 × Porb around background star. (i, qshort, simulated star)
-        NTP No unresolved companion. Transiting planet with Porb around nearby star. (i, Rp)
-        NEB No unresolved companion. Eclipsing binary with Porb around nearby star. (i, qshort)
-        NEBx2P No unresolved companion. Eclipsing binary with 2 × Porb around nearby star. (i, qshort)
-        FPP = 1 - (TP + PTP + DTP)
-        NFPP = NTP + NEB + NEBx2P
-        Giacalone & Dressing (2020) define validated planets as TOIs with NFPP < 10−3 and FPP < 0.015 (or FPP ≤ 0.01,
-        when rounding to the nearest percent)
-        @param cpus: number of cpus to be used
-        @param indir: root directory to store the results
-        @param tic: the tess object id for which the analysis will be run
-        @param sectors: the sectors of the tic
-        @param lc_file: the light curve source file
-        @param transit_depth: the depth of the transit signal (ppts)
-        @param period: the period of the transit signal /days)
-        @param t0: the t0 of the transit signal (days)
-        @param transit_duration: the duration of the transit signal (minutes)
-        """
-        save_dir = indir + "/" + str(tic) + "/triceratops_" + str(uuid.uuid4())
-        if os.path.exists(save_dir):
-            shutil.rmtree(save_dir, ignore_errors=True)
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        sectors = np.array(sectors)
-        duration = transit_duration / 60 / 24
-        target = tr.target(ID=tic, sectors=sectors)
-        sectors = sectors[sectors <= maxsector.maxsector]
-        if len(sectors) == 0:
-            logging.warning("There are no available sectors to be validated, skipping TRICERATOPS.")
-            return save_dir, None, None
-        logging.info("Will execute validation for sectors: " + str(sectors))
-        # TODO allow user input apertures
-        tpfs = lightkurve.search_targetpixelfile("TIC " + str(tic), mission="TESS", cadence="short", sector=sectors.tolist())\
-            .download_all()
-        star = eleanor.multi_sectors(tic=tic, sectors=sectors, tesscut_size=31, post_dir=const.USER_HOME_ELEANOR_CACHE)
-        apertures = []
-        sector_num = 0
-        logging.info("Calculating validation masks")
-        for s in star:
-            tpf_idx = [data.sector if data.sector == s.sector else -1 for data in tpfs.data]
-            tpf = tpfs[np.where(tpf_idx > np.zeros(len(tpf_idx)))[0][0]]
-            pipeline_mask = tpfs[np.where(tpf_idx > np.zeros(len(tpf_idx)))[0][0]].pipeline_mask
-            pipeline_mask = np.transpose(pipeline_mask)
-            pipeline_mask_triceratops = np.zeros((len(pipeline_mask[0]), len(pipeline_mask[:][0]), 2))
-            for i in range(0, len(pipeline_mask[0])):
-                for j in range(0, len(pipeline_mask[:][0])):
-                    pipeline_mask_triceratops[i, j] = [tpf.column + i, tpf.row + j]
-            pipeline_mask_triceratops[~pipeline_mask] = None
-            aperture = []
-            for i in range(0, len(pipeline_mask_triceratops[0])):
-                for j in range(0, len(pipeline_mask_triceratops[:][0])):
-                    if not np.isnan(pipeline_mask_triceratops[i, j]).any():
-                        aperture.append(pipeline_mask_triceratops[i, j])
-            apertures.append(aperture)
-            logging.info("Saving validation mask plot for sector %s", s.sector)
-            target.plot_field(save=True, fname=save_dir + "/field_S" + str(s.sector), sector=s.sector,
-                            ap_pixels=aperture)
-            sector_num = sector_num + 1
-        apertures = np.array(apertures)
-        depth = transit_depth / 1000
-        logging.info("Calculating validation closest stars depths")
-        target.calc_depths(depth, apertures)
-        target.stars.to_csv(save_dir + "/stars.csv", index=False)
-        lc = pd.read_csv(lc_file, header=0)
-        time, flux, flux_err = lc["#time"].values, lc["flux"].values, lc["flux_err"].values
-        lc_len = len(time)
-        zeros_lc = np.zeros(lc_len)
-        logging.info("Preparing validation light curve for target")
-        lc = TessLightCurve(time=time, flux=flux, flux_err=flux_err, quality=zeros_lc)
-        lc.extra_columns = []
-        lc = lc.fold(period=period, epoch_time=t0, normalize_phase=True)
-        folded_plot_range = duration / 2 / period * 5
-        inner_folded_range_args = np.where((0 - folded_plot_range < lc.time.value) & (lc.time.value < 0 + folded_plot_range))
-        lc = lc[inner_folded_range_args]
-        lc.time = lc.time * period
-        sigma = np.mean(lc.flux_err)
-        logging.info("Preparing validation processes inputs")
-        input_n_times = [ValidatorInput(save_dir, copy.deepcopy(target), lc.time.value, lc.flux.value, sigma, period, depth,
-                                        apertures, value)
-                         for value in range(0, self.validation_runs)]
-        validator = Validator()
-        logging.info("Start validation processes")
-        with Pool(processes=cpus) as pool:
-            validation_results = pool.map(validator.validate, input_n_times)
-        logging.info("Finished validation processes")
-        fpp_sum = 0
-        nfpp_sum = 0
-        probs_total_df = None
-        scenarios_num = len(validation_results[0][2])
-        star_num = np.zeros((5, scenarios_num))
-        u1 = np.zeros((5, scenarios_num))
-        u2 = np.zeros((5, scenarios_num))
-        fluxratio_EB = np.zeros((5, scenarios_num))
-        fluxratio_comp = np.zeros((5, scenarios_num))
-        target = input_n_times[0].target
-        target.star_num = np.zeros(scenarios_num)
-        target.u1 = np.zeros(scenarios_num)
-        target.u2 = np.zeros(scenarios_num)
-        target.fluxratio_EB = np.zeros(scenarios_num)
-        target.fluxratio_comp = np.zeros(scenarios_num)
-        logging.info("Computing final probabilities from the %s scenarios", self.validation_runs)
-        i = 0
-        for fpp, nfpp, probs_df, star_num_arr, u1_arr, u2_arr, fluxratio_EB_arr, fluxratio_comp_arr in validation_results:
-            if probs_total_df is None:
-                probs_total_df = probs_df
-            else:
-                probs_total_df = pd.concat((probs_total_df, probs_df))
-            fpp_sum = fpp_sum + fpp
-            nfpp_sum = nfpp_sum + nfpp
-            star_num[i] = star_num_arr
-            u1[i] = u1_arr
-            u2[i] = u2_arr
-            fluxratio_EB[i] = fluxratio_EB_arr
-            fluxratio_comp[i] = fluxratio_comp_arr
-            i = i + 1
-        for i in range(0, scenarios_num):
-            target.star_num[i] = np.mean(star_num[:, i])
-            target.u1[i] = np.mean(u1[:, i])
-            target.u2[i] = np.mean(u2[:, i])
-            target.fluxratio_EB[i] = np.mean(fluxratio_EB[:, i])
-            target.fluxratio_comp[i] = np.mean(fluxratio_comp[:, i])
-        with open(save_dir + "/validation.csv", 'w') as the_file:
-            the_file.write("FPP,NFPP\n")
-            the_file.write(str(fpp_sum / self.validation_runs) + "," + str(nfpp_sum / self.validation_runs))
-        probs_total_df = probs_total_df.groupby("scenario", as_index=False).mean()
-        probs_total_df["scenario"] = pd.Categorical(probs_total_df["scenario"], ["TP", "EB", "EBx2P", "PTP", "PEB", "PEBx2P",
-                                                                         "STP", "SEB", "SEBx2P", "DTP", "DEB", "DEBx2P",
-                                                                         "BTP", "BEB", "BEBx2P", "NTP", "NEB", "NEBx2P"])
-        probs_total_df = probs_total_df.sort_values("scenario")
-        probs_total_df.to_csv(save_dir + "/validation_scenarios.csv", index=False)
-        target.probs = probs_total_df
-        # target.plot_fits(save=True, fname=save_dir + "/scenario_fits", time=lc.time.value, flux_0=lc.flux.value,
-        #                  flux_err_0=sigma)
-        return save_dir, star[0].coords[0], star[0].coords[1]
 
     @staticmethod
     def plot_fits(target, save_file, time: np.ndarray, flux_0: np.ndarray, sigma_0: float):
