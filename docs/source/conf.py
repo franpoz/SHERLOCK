3ae7f9c066b3f162adcaa065c53d85f18be27f50
martin.dvpj <martin.devora.pajares@gmail.com>
2021-07-09 12:54:26 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-import sphinxcontrib.mermaid
 
 # -- Project information -----------------------------------------------------
 
