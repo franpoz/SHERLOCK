7835157308dbc5f805d1fbbbbb961c1b831b72b4
martin.dvpj <martin.devora.pajares@gmail.com>
2021-07-16 22:33:32 +0100

    stability planet_input.py update

--- sherlockpipe/nbodies/planet_input.py
+++ sherlockpipe/nbodies/planet_input.py
@@ -2,14 +2,30 @@ class PlanetInput:
     """
     Defines the planet parameters for system stability calculations
     """
-    def __init__(self, period, radius, mass_low=None, mass_max=None, eccentricity_low=0, eccentricity_up=0, omega=0,
-                 mass_bins=3, ecc_bins=3):
+    def __init__(self, period, period_low_err, period_up_err,
+                 radius, radius_low_err, radius_up_err, eccentricity, ecc_low_err, ecc_up_err,
+                 inclination, inc_low_err, inc_up_err, omega, omega_low_err, omega_up_err,
+                 mass=None, mass_low_err=None, mass_up_err=None,
+                 mass_bins=3, ecc_bins=3, inc_bins=3, omega_bins=3):
         self.period = period
+        self.period_low_err = period_low_err
+        self.period_up_err = period_up_err
         self.radius = radius
-        self.eccentricity_low = eccentricity_low
-        self.eccentricity_up = eccentricity_up
+        self.radius_low_err = radius_low_err
+        self.radius_up_err = radius_up_err
+        self.eccentricity = eccentricity
+        self.ecc_low_err = ecc_low_err
+        self.ecc_up_err = ecc_up_err
+        self.inclination = inclination
+        self.inc_low_err = inc_low_err
+        self.inc_up_err = inc_up_err
         self.omega = omega
-        self.mass_low = mass_low
-        self.mass_up = mass_max
+        self.omega_low_err = omega_low_err
+        self.omega_up_err = omega_up_err
+        self.mass = mass
+        self.mass_low_err = mass_low_err
+        self.mass_up_err = mass_up_err
         self.mass_bins = mass_bins if mass_bins is not None else 3
         self.ecc_bins = ecc_bins if ecc_bins is not None else 3
+        self.inc_bins = inc_bins if inc_bins is not None else 3
+        self.omega_bins = omega_bins if omega_bins is not None else 3
