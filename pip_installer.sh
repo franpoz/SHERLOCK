1fbada8532a4dec62d6fe17286a4f41fe0da0ef8
martin.dvpj <martin.devora.pajares@gmail.com>
2021-08-16 23:15:51 +0100

    also adding version into init

--- pip_installer.sh
+++ pip_installer.sh
@@ -8,6 +8,7 @@ tests_results=$(cat tests.log | grep "congratulations")
 if ! [[ -z ${tests_results} ]]; then
   git_tag=$1
   sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
+  sed -i '1s/.*/__version__ = "'${git_tag}'"/' sherlockpipe/__init__.py
   git add setup.py
   git commit -m "Preparing release ${git_tag}"
   git tag ${git_tag} -m "New release"
