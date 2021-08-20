38af1e6a6be9c3389b4406bfc18f260e0a89b2df
martin.dvpj <martin.devora.pajares@gmail.com>
2021-08-20 16:44:05 +0100

    new version

--- pip_installer.sh
+++ pip_installer.sh
@@ -10,6 +10,7 @@ if ! [[ -z ${tests_results} ]]; then
   sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
   sed -i '1s/.*/__version__ = "'${git_tag}'"/' sherlockpipe/__init__.py
   git add setup.py
+  git sherlockpipe/__init__.py
   git commit -m "Preparing release ${git_tag}"
   git tag ${git_tag} -m "New release"
   git push
