8ab13bda827c88bf03b4627ea4c5f08a3055c222
martin.dvpj <martin.devora.pajares@gmail.com>
2021-08-20 13:59:35 +0100

    Preparing release 0.24.1-RC11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-version = "0.24.1-RC10"
+version = "0.24.1-RC11"
 
 setuptools.setup(
     name="sherlockpipe", # Replace with your own username
@@ -26,34 +26,34 @@ setuptools.setup(
                         "astroquery==0.4.1",
                         "allesfitter==1.2.2",
                         'argparse==1.4.0',
-                        "celerite==0.4.0", # Allesfitter dependency
-                        "cython==0.29.21",
-                        "extension-helpers==0.1",
-                        "requests==2.25.1",
-                        "matplotlib==3.3.4",
-                        "pyyaml==5.4.1",
-                        "dynesty==1.0.1",
-                        "emcee==3.0.2",
-                        "corner==2.1.0",
-                        "ellc==1.8.5",
-                        "imageio==2.9.0",
-                        "seaborn==0.11.1",
-                        "bokeh==2.2.3",
-                        "sklearn==0.0",
-                        "reproject==0.4",
-                        "reportlab==3.5.59",
-                        "rebound==3.17.3",
-                        "spock==1.3.1",
-                        "lcbuilder==0.5.3",
-                        "mock==4.0.3",
-                        'tqdm==4.56.0',
-                        'setuptools>=41.0.0',
-                        'torch==1.9.0',
                         'beautifulsoup4==4.9.3',
-                        'numba>=0.53.0rc1',
+                        "bokeh==2.2.3", # TPFPlotter dependency
+                        "celerite==0.4.0", # Allesfitter dependency
                         'configparser==5.0.1',
+                        "corner==2.1.0", # Allesfitter dependency
+                        "cython==0.29.21",
+                        "dynesty==1.0.1", # Allesfitter dependency
+                        "ellc==1.8.5", # Allesfitter dependency
+                        "emcee==3.0.2", # Allesfitter dependency
+                        "extension-helpers==0.1",
+                        "imageio==2.9.0",
+                        "lcbuilder==0.5.4",
+                        "matplotlib==3.3.4",
+                        "mock==4.0.3",
+                        'numba==0.53.1', # foldedleastsquares dependency
                         'pyparsing==2.4.7', # Matplotlib dependency
+                        "pyyaml==5.4.1",
+                        "requests==2.25.1",
+                        "rebound==3.17.3",
+                        "reportlab==3.5.59",
+                        "reproject==0.4",
+                        "seaborn==0.11.1",
+                        'setuptools>=41.0.0',
+                        "sklearn==0.0",
+                        "spock==1.3.1",
                         'statsmodels==0.12.2', # Allesfitter dependency
+                        'torch==1.9.0',
+                        'tqdm==4.56.0',
                         'triceratops==1.0.10',
                         'uncertainties==3.1.5'
     ]
