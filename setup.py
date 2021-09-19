021f5c1638a377d4f5d51451ef30c1dc3a18f130
martin.dvpj <martin.devora.pajares@gmail.com>
2021-09-19 21:17:11 +0100

    update lcbuilder

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
                         "emcee==3.0.2", # Allesfitter dependency
                         "extension-helpers==0.1",
                         "imageio==2.9.0",
-                        "lcbuilder==0.5.8",
+                        "lcbuilder==0.5.9",
                         "matplotlib==3.3.4",
                         "mock==4.0.3",
                         'numba==0.53.1', # foldedleastsquares dependency
