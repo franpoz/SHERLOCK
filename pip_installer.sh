cf32c4cba0919e1e5e507e4042c1bc62820a297c
martin.dvpj <martin.devora.pajares@gmail.com>
2021-12-15 11:26:50 +0000

    improve pip_installer.sh

--- pip_installer.sh
+++ pip_installer.sh
@@ -3,9 +3,9 @@
 rm tests.log
 rm dist* -r
 set -e
-#tox -r > tests.log
-#tests_results=$(cat tests.log | grep "congratulations")
-#if ! [[ -z ${tests_results} ]]; then
+tox -r > tests.log
+tests_results=$(cat tests.log | grep "congratulations")
+if ! [[ -z ${tests_results} ]]; then
   git_tag=$1
   sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
   sed -i '1s/.*/__version__ = "'${git_tag}'"/' sherlockpipe/__init__.py
@@ -14,8 +14,10 @@ set -e
   git commit -m "Preparing release ${git_tag}"
   git tag ${git_tag} -m "New release"
   git push
+  git push --tags
   python3 setup.py sdist bdist_wheel
   python3 -m twine upload dist/*
+  rm dist* -r
   echo "Build docker image"
   sudo docker build ./docker/ --no-cache
   docker_image_id=$(sudo docker images | awk '{print $3}' | awk 'NR==2')
@@ -25,7 +27,7 @@ set -e
   echo "Push docker image with tag ${git_tag}"
   sudo docker push sherlockpipe/sherlockpipe:latest
   sudo docker push sherlockpipe/sherlockpipe:${git_tag}
-#else
-#  echo "TESTS FAILED. See tests.log"
-#fi
-rm dist* -r
+else
+  echo "TESTS FAILED. See tests.log"
+fi
+
