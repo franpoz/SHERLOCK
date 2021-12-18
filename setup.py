80791c3ce51d9078b2c2644c935108b119c44a17
martin.dvpj <martin.devora.pajares@gmail.com>
2021-12-18 08:41:48 +0000

    Preparing release 0.27.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.27.0"
+version = "0.27.3"
 setuptools.setup(
     name="sherlockpipe", # Replace with your own username
     version=version,
