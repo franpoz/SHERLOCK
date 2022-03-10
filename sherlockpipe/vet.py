a040b691a51d0021fbdf2f61f3230f2070203588
martin.dvpj <martin.devora.pajares@gmail.com>
2022-03-10 23:13:37 +0000

    update vet

--- sherlockpipe/vet.py
+++ sherlockpipe/vet.py
@@ -3,10 +3,10 @@ import multiprocessing
 import yaml
 from argparse import ArgumentParser
 import sys
-from patoso.patoso import Patoso
 import pandas as pd
 import os
 
+from watson.watson import Watson
 
 if __name__ == '__main__':
     ap = ArgumentParser(description='Vetting of Sherlock objects of interest')
@@ -16,8 +16,8 @@ if __name__ == '__main__':
     ap.add_argument('--properties', help="The YAML file to be used as input.", required=False)
     ap.add_argument('--cpus', type=int, default=None, help="The number of CPU cores to be used.", required=False)
     args = ap.parse_args()
-    patoso = Patoso(args.object_dir)
-    file_dir = patoso.object_dir + "/vetting.log"
+    watson = Watson(args.object_dir)
+    file_dir = watson.object_dir + "/vetting.log"
     if os.path.exists(file_dir):
         os.remove(file_dir)
     formatter = logging.Formatter('%(message)s')
@@ -34,7 +34,8 @@ if __name__ == '__main__':
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logging.info("Starting vetting")
-    star_df = pd.read_csv(patoso.object_dir + "/params_star.csv")
+    star_df = pd.read_csv(watson.object_dir + "/params_star.csv")
+    transits_df = None
     if args.candidate is None:
         user_properties = yaml.load(open(args.properties), yaml.SafeLoader)
         candidate = pd.DataFrame(columns=['id', 'period', 'depth', 't0', 'sectors', 'ffi', 'number', 'lc'])
@@ -42,16 +43,18 @@ if __name__ == '__main__':
         candidate['id'] = star_df.iloc[0]["obj_id"]
     else:
         candidate_selection = int(args.candidate)
-        candidates = pd.read_csv(patoso.object_dir + "/candidates.csv")
+        candidates = pd.read_csv(watson.object_dir + "/candidates.csv")
         if candidate_selection < 1 or candidate_selection > len(candidates.index):
             raise SystemExit("User selected a wrong candidate number.")
         candidates = candidates.rename(columns={'Object Id': 'id'})
         candidate = candidates.iloc[[candidate_selection - 1]]
         candidate['number'] = [candidate_selection]
-        patoso.data_dir = patoso.object_dir
+        transits_df = pd.read_csv(watson.object_dir + "/transits_stats.csv")
+        transits_df = transits_df[transits_df["candidate"] == candidate_selection - 1]
+        watson.data_dir = watson.object_dir
         logging.info("Selected signal number " + str(candidate_selection))
     if args.cpus is None:
         cpus = multiprocessing.cpu_count() - 1
     else:
         cpus = args.cpus
-    patoso.vetting_with_data(candidate, star_df.iloc[0], cpus)
+    watson.vetting_with_data(candidate, star_df.iloc[0], transits_df, cpus)
