88158838844b8ce2ac8d38f10d3d421ef4ac6637
martin.dvpj <martin.devora.pajares@gmail.com>
2022-03-12 17:45:11 +0000

    update version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
                         "emcee==3.0.2", # Allesfitter dependency
                         "extension-helpers==0.1",
                         "imageio==2.9.0",
-                        "lcbuilder==0.7.15",
+                        "lcbuilder==0.7.16",
                         "matplotlib==3.3.4",
                         "mock==4.0.3",
                         'numba==0.53.1', # foldedleastsquares dependency
