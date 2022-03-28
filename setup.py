56b46622fba93bc21dc182a1082e9a2730713cbd
martin.dvpj <martin.devora.pajares@gmail.com>
2022-03-28 22:08:57 +0100

    update watson

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
                         "celerite==0.4.0", # Allesfitter dependency
                         "corner==2.1.0", # Allesfitter dependency
                         "cython==0.29.21",
-                        "dearwatson==0.1.12", # Vetting
+                        "dearwatson==0.1.14", # Vetting
                         "dynesty==1.0.1", # Allesfitter dependency
                         "ellc==1.8.5", # Allesfitter dependency
                         "emcee==3.0.2", # Allesfitter dependency
