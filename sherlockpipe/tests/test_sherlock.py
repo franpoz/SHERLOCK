c36d11e9f1b73db05f7353d962762f711a1b161d
martin.dvpj <martin.devora.pajares@gmail.com>
2022-05-09 22:46:26 +0100

    fix test

--- sherlockpipe/tests/test_sherlock.py
+++ sherlockpipe/tests/test_sherlock.py
@@ -164,7 +164,7 @@ class TestsSherlock(unittest.TestCase):
                                      detrends_number=1, max_runs=1, oversampling=0.05)], True).run()
             run_dir = "TIC259377017_[5]"
             self.assertTrue(os.path.exists(run_dir))
-            self.assertTrue(os.path.exists(run_dir + "/Phase_detrend_period_TIC259377017_[5]_0.52_days.png"))
+            self.assertTrue(os.path.exists(run_dir + '/Phase_detrend_period_TIC259377017_[5]_0.25_days.png'))
         finally:
             self.__clean(run_dir)
 
