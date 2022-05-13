3d877c49b8cd4e1cb01b7123de8fa254e57f6dc6
martin.dvpj <martin.devora.pajares@gmail.com>
2022-05-13 22:36:23 +0100

    update

--- sherlockpipe/ml/DETREND.py
+++ sherlockpipe/ml/DETREND.py
@@ -7,57 +7,79 @@ class AutoEncoder():
         # (flux, centroidx, centroidy, motionx, motiony, bck)
         input_size = 20610
         input = keras.Input(shape=(20610, 6))
-        self.enc_branch1 = keras.layers.SpatialDropout1D(rate=0.2)(input)
-        self.enc_branch1_r = keras.layers.Conv1D(filters=5000, kernel_size=100, padding="same", activation="relu")(self.enc_branch1)
-        self.enc_branch1 = keras.layers.MaxPooling1D(pool_size=50, strides=10, padding="same")(self.enc_branch1_r)
-        self.enc_branch1 = keras.layers.Dropout(rate=0.1)(self.enc_branch1)
-        self.enc_branch2_r = keras.layers.Conv1D(filters=1250, kernel_size=33, padding="same", activation="relu")(self.enc_branch1)
-        self.enc_branch2 = keras.layers.MaxPooling1D(pool_size=20, strides=5, padding="same")(self.enc_branch2_r)
-        self.enc_branch2 = keras.layers.Dropout(rate=0.1)(self.enc_branch2)
-        self.enc_branch3_r = keras.layers.Conv1D(filters=420, kernel_size=15, padding="same", activation="relu")(self.enc_branch2)
-        self.enc_branch3 = keras.layers.MaxPooling1D(pool_size=15, strides=5, padding="same")(self.enc_branch3_r)
-        self.enc_branch3 = keras.layers.Dropout(rate=0.1)(self.enc_branch3)
-        self.enc_branch4_r = keras.layers.Conv1D(filters=128, kernel_size=9, padding="same", activation="relu")(self.enc_branch3)
-        self.enc_branch4 = keras.layers.MaxPooling1D(pool_size=10, strides=2, padding="same")(self.enc_branch4_r)
-        self.enc_branch4 = keras.layers.Dropout(rate=0.1)(self.enc_branch4)
-        self.enc_branch5_r = keras.layers.Conv1D(filters=64, kernel_size=7, padding="same", activation="relu")(self.enc_branch4)
-        self.enc_branch5 = keras.layers.MaxPooling1D(pool_size=4, strides=2, padding="same")(self.enc_branch5_r)
-        self.enc_branch5 = keras.layers.Dropout(rate=0.1)(self.enc_branch5)
-        self.enc_branch6_r = keras.layers.Conv1D(filters=32, kernel_size=5, padding="same", activation="relu")(self.enc_branch5)
-        self.enc_branch6 = keras.layers.MaxPooling1D(pool_size=3, strides=1, padding="same")(self.enc_branch6_r)
-        self.enc_branch6 = keras.layers.Dropout(rate=0.1)(self.enc_branch6)
-        self.enc_branch7_r = keras.layers.Conv1D(filters=16, kernel_size=3, padding="same", activation="relu")(self.enc_branch6)
-        self.enc_branch7 = keras.layers.MaxPooling1D(pool_size=2, strides=1, padding="same")(self.enc_branch7_r)
-        self.enc_branch7 = keras.layers.Dropout(rate=0.1)(self.enc_branch7)
-        self.dec_branch7 = keras.layers.UpSampling1D(1)(self.enc_branch7)
-        self.dec_branch7 = keras.layers.Conv1DTranspose(filters=16, kernel_size=3, padding="same")(self.enc_branch7)
-        self.dec_branch6 = keras.layers.UpSampling1D(1)(self.dec_branch7)
-        self.dec_branch6 = keras.layers.Conv1DTranspose(filters=32, kernel_size=5, padding="same")(self.dec_branch6)
-        self.dec_branch6 = keras.layers.Add()([self.enc_branch6_r, self.dec_branch6])
-        self.dec_branch5 = keras.layers.UpSampling1D(2)(self.dec_branch6)
-        self.dec_branch5 = keras.layers.Conv1DTranspose(filters=64, kernel_size=7, padding="same")(self.dec_branch5)
-        self.dec_branch5 = keras.layers.Add()([self.enc_branch5_r, self.dec_branch5])
-        self.dec_branch4 = keras.layers.UpSampling1D(2)(self.dec_branch5)
-        self.dec_branch4 = keras.layers.Cropping1D(cropping=(0, 1))(self.dec_branch4)
-        self.dec_branch4 = keras.layers.Conv1DTranspose(filters=128, kernel_size=9, padding="same")(self.dec_branch4)
-        self.dec_branch4 = keras.layers.Add()([self.enc_branch4_r, self.dec_branch4])
-        self.dec_branch3 = keras.layers.UpSampling1D(5)(self.dec_branch4)
-        self.dec_branch3 = keras.layers.Cropping1D(cropping=1)(self.dec_branch3)
-        self.dec_branch3 = keras.layers.Conv1DTranspose(filters=420, kernel_size=15, padding="same")(self.dec_branch3)
-        self.dec_branch3 = keras.layers.Add()([self.enc_branch3_r, self.dec_branch3])
-        self.dec_branch2 = keras.layers.UpSampling1D(5)(self.dec_branch3)
-        self.dec_branch2 = keras.layers.Cropping1D(cropping=2)(self.dec_branch2)
-        self.dec_branch2 = keras.layers.Conv1DTranspose(filters=1250, kernel_size=33, padding="same")(self.dec_branch2)
-        self.dec_branch2 = keras.layers.Add()([self.enc_branch2_r, self.dec_branch2])
-        self.dec_branch1 = keras.layers.UpSampling1D(10)(self.dec_branch2)
-        self.dec_branch1 = keras.layers.Conv1DTranspose(filters=5000, kernel_size=100, padding="same")(self.dec_branch2)
-        self.dec_branch1 = keras.layers.Add()([self.enc_branch1_r, self.dec_branch1])
+        autoencoder_layer1_strides = 10
+        autoencoder_layer1_filters = 5000
+        autoencoder_layer1_ks = 100
+        autoencoder_layer2_strides = 5
+        autoencoder_layer2_filters = 1250
+        autoencoder_layer2_ks = 33
+        autoencoder_layer3_strides = 5
+        autoencoder_layer3_filters = 420
+        autoencoder_layer3_ks = 15
+        autoencoder_layer4_strides = 2
+        autoencoder_layer4_filters = 128
+        autoencoder_layer4_ks = 9
+        autoencoder_layer5_strides = 2
+        autoencoder_layer5_filters = 64
+        autoencoder_layer5_ks = 7
+        autoencoder_layer6_strides = 1
+        autoencoder_layer6_filters = 32
+        autoencoder_layer6_ks = 5
+        autoencoder_layer7_strides = 1
+        autoencoder_layer7_filters = 16
+        autoencoder_layer7_ks = 3
+        self.enc_layer1 = keras.layers.SpatialDropout1D(rate=0.2)(input)
+        self.enc_layer1_r = keras.layers.Conv1D(filters=autoencoder_layer1_filters, kernel_size=autoencoder_layer1_ks, padding="same", activation="relu")(self.enc_layer1)
+        self.enc_layer1 = keras.layers.MaxPooling1D(pool_size=50, strides=autoencoder_layer1_strides, padding="same")(self.enc_layer1_r)
+        self.enc_layer1 = keras.layers.Dropout(rate=0.1)(self.enc_layer1)
+        self.enc_layer2_r = keras.layers.Conv1D(filters=autoencoder_layer2_filters, kernel_size=autoencoder_layer2_ks, padding="same", activation="relu")(self.enc_layer1)
+        self.enc_layer2 = keras.layers.MaxPooling1D(pool_size=20, strides=autoencoder_layer2_strides, padding="same")(self.enc_layer2_r)
+        self.enc_layer2 = keras.layers.Dropout(rate=0.1)(self.enc_layer2)
+        self.enc_layer3_r = keras.layers.Conv1D(filters=autoencoder_layer3_filters, kernel_size=autoencoder_layer3_ks, padding="same", activation="relu")(self.enc_layer2)
+        self.enc_layer3 = keras.layers.MaxPooling1D(pool_size=15, strides=autoencoder_layer3_strides, padding="same")(self.enc_layer3_r)
+        self.enc_layer3 = keras.layers.Dropout(rate=0.1)(self.enc_layer3)
+        self.enc_layer4_r = keras.layers.Conv1D(filters=autoencoder_layer4_filters, kernel_size=autoencoder_layer4_ks, padding="same", activation="relu")(self.enc_layer3)
+        self.enc_layer4 = keras.layers.MaxPooling1D(pool_size=10, strides=autoencoder_layer4_strides, padding="same")(self.enc_layer4_r)
+        self.enc_layer4 = keras.layers.Dropout(rate=0.1)(self.enc_layer4)
+        self.enc_layer5_r = keras.layers.Conv1D(filters=autoencoder_layer5_filters, kernel_size=autoencoder_layer5_ks, padding="same", activation="relu")(self.enc_layer4)
+        self.enc_layer5 = keras.layers.MaxPooling1D(pool_size=4, strides=autoencoder_layer5_strides, padding="same")(self.enc_layer5_r)
+        self.enc_layer5 = keras.layers.Dropout(rate=0.1)(self.enc_layer5)
+        self.enc_layer6_r = keras.layers.Conv1D(filters=autoencoder_layer6_filters, kernel_size=autoencoder_layer6_ks, padding="same", activation="relu")(self.enc_layer5)
+        self.enc_layer6 = keras.layers.MaxPooling1D(pool_size=3, strides=autoencoder_layer6_strides, padding="same")(self.enc_layer6_r)
+        self.enc_layer6 = keras.layers.Dropout(rate=0.1)(self.enc_layer6)
+        self.enc_layer7_r = keras.layers.Conv1D(filters=autoencoder_layer7_filters, kernel_size=autoencoder_layer7_ks, padding="same", activation="relu")(self.enc_layer6)
+        self.enc_layer7 = keras.layers.MaxPooling1D(pool_size=2, strides=autoencoder_layer7_strides, padding="same")(self.enc_layer7_r)
+        self.enc_layer7 = keras.layers.Dropout(rate=0.1)(self.enc_layer7)
+        self.dec_layer7 = keras.layers.UpSampling1D(autoencoder_layer7_strides)(self.enc_layer7)
+        self.dec_layer7 = keras.layers.Conv1DTranspose(filters=autoencoder_layer7_filters, kernel_size=autoencoder_layer7_ks, padding="same")(self.enc_layer7)
+        self.dec_layer6 = keras.layers.UpSampling1D(autoencoder_layer6_strides)(self.dec_layer7)
+        self.dec_layer6 = keras.layers.Conv1DTranspose(filters=autoencoder_layer6_filters, kernel_size=autoencoder_layer6_ks, padding="same")(self.dec_layer6)
+        self.dec_layer6 = keras.layers.Add()([self.enc_layer6_r, self.dec_layer6])
+        self.dec_layer5 = keras.layers.UpSampling1D(autoencoder_layer5_strides)(self.dec_layer6)
+        self.dec_layer5 = keras.layers.Conv1DTranspose(filters=autoencoder_layer5_filters, kernel_size=autoencoder_layer5_ks, padding="same")(self.dec_layer5)
+        self.dec_layer5 = keras.layers.Add()([self.enc_layer5_r, self.dec_layer5])
+        self.dec_layer4 = keras.layers.UpSampling1D(autoencoder_layer4_strides)(self.dec_layer5)
+        self.dec_layer4 = keras.layers.Cropping1D(cropping=(0, 1))(self.dec_layer4)
+        self.dec_layer4 = keras.layers.Conv1DTranspose(filters=autoencoder_layer4_filters, kernel_size=autoencoder_layer4_ks, padding="same")(self.dec_layer4)
+        self.dec_layer4 = keras.layers.Add()([self.enc_layer4_r, self.dec_layer4])
+        self.dec_layer3 = keras.layers.UpSampling1D(autoencoder_layer3_strides)(self.dec_layer4)
+        self.dec_layer3 = keras.layers.Cropping1D(cropping=1)(self.dec_layer3)
+        self.dec_layer3 = keras.layers.Conv1DTranspose(filters=autoencoder_layer3_filters, kernel_size=autoencoder_layer3_ks, padding="same")(self.dec_layer3)
+        self.dec_layer3 = keras.layers.Add()([self.enc_layer3_r, self.dec_layer3])
+        self.dec_layer2 = keras.layers.UpSampling1D(autoencoder_layer2_strides)(self.dec_layer3)
+        self.dec_layer2 = keras.layers.Cropping1D(cropping=2)(self.dec_layer2)
+        self.dec_layer2 = keras.layers.Conv1DTranspose(filters=autoencoder_layer2_filters, kernel_size=autoencoder_layer2_ks, padding="same")(self.dec_layer2)
+        self.dec_layer2 = keras.layers.Add()([self.enc_layer2_r, self.dec_layer2])
+        self.dec_layer1 = keras.layers.UpSampling1D(autoencoder_layer1_strides)(self.dec_layer2)
+        self.dec_layer1 = keras.layers.Conv1DTranspose(filters=autoencoder_layer1_filters, kernel_size=autoencoder_layer1_ks, padding="same")(self.dec_layer2)
+        self.dec_layer1 = keras.layers.Add()([self.enc_layer1_r, self.dec_layer1])
         self.linear_proj = keras.layers.GlobalAveragePooling1D()(self.dec_branch1)
         self.linear_proj = keras.layers.Dense(input_size)(self.linear_proj, activation="relu")
         self.linear_proj = keras.layers.Dropout(rate=0.1)(self.linear_proj)
         self.linear_proj = keras.layers.Dense(input_size)(self.linear_proj, activation="softmax")
         model = Model(inputs=input, outputs=self.linear_proj)
         keras.utils.vis_utils.plot_model(model, "detrend_autoencoder_resnet.png", show_shapes=True)
+
 
 AutoEncoder().build()
 
