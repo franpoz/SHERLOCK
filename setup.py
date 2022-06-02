5a91946afb50eecf54e31f2ef534eea529804a39
martin.dvpj <martin.devora.pajares@gmail.com>
2022-06-02 22:29:19 +0100

    update watson

--- setup.py
+++ setup.py
@@ -23,30 +23,30 @@ setuptools.setup(
     python_requires='>=3.8',
     install_requires=['astroplan==0.7',
                         "astroquery==0.4.6",
-                        "allesfitter==1.2.9",
-                        'argparse==1.4.0',
+                        "allesfitter==1.2.9", # Fit
+                        'argparse==1.4.0', # All modules
                         'beautifulsoup4==4.9.3', # Parsing HTML and XML, for OIs extraction
                         "celerite==0.4.0", # Allesfitter dependency
                         "corner==2.1.0", # Allesfitter dependency
                         "Cython==0.29.21",
-                        "dearwatson==0.2.7", # Vetting
+                        "dearwatson==0.2.8", # Vetting
                         "dynesty==1.0.1", # Allesfitter dependency
                         "ellc==1.8.5", # Allesfitter dependency
                         "emcee==3.0.2", # Allesfitter dependency
                         "mock==4.0.3",
                         'numba==0.53.1', # foldedleastsquares dependency
                         'pytz', # Observation plan: Not using version because it gets the DB updated with each release
-                        "requests==2.25.1",
-                        "rebound==3.17.3",
+                        "requests==2.25.1", # OIs management
+                        "rebound==3.17.3", # Stability
                         "reproject==0.4",
                         "seaborn==0.11.1",
                         'setuptools>=41.0.0',
                         "sklearn==0.0",
-                        "spock==1.3.1",
+                        "spock==1.3.1", # Stability
                         'statsmodels==0.12.2', # Allesfitter dependency
                         'timezonefinder==5.2.0', # Observation plan
                         'tqdm==4.56.0',
-                        'triceratops==1.0.15',
-                        'uncertainties==3.1.5'
+                        'triceratops==1.0.15', # Validation
+                        'uncertainties==3.1.5' # Observation plan
     ]
 )
