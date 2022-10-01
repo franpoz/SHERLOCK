c1e4e4d3962f8d4ece48381593734a66bafc50a3
martin.dvpj <martin.devora.pajares@gmail.com>
2022-10-01 20:20:38 +0100

    building in github

--- pip_installer.sh
+++ pip_installer.sh
@@ -18,19 +18,19 @@ if ! [[ -z ${tests_results} ]]; then
   git tag ${git_tag} -m "New release"
   git push
   git push --tags
-  python3 setup.py sdist bdist_wheel
-  python3 -m twine upload dist/*
-  echo "Build docker image"
-  sudo docker build ./docker/ --no-cache
-  docker_image_id=$(sudo docker images | awk '{print $3}' | awk 'NR==2')
-  echo "Tagging docker image with tag ${git_tag}"
-  sudo docker tag ${docker_image_id} sherlockpipe/sherlockpipe:latest
-  sudo docker tag ${docker_image_id} sherlockpipe/sherlockpipe:${git_tag}
-  echo "Push docker image with tag ${git_tag}"
-  sudo docker push sherlockpipe/sherlockpipe:latest
-  sudo docker push sherlockpipe/sherlockpipe:${git_tag}
-  sudo docker images prune -all
-  rm tests.log
+#  python3 setup.py sdist bdist_wheel
+#  python3 -m twine upload dist/*
+#  echo "Build docker image"
+#  sudo docker build ./docker/ --no-cache
+#  docker_image_id=$(sudo docker images | awk '{print $3}' | awk 'NR==2')
+#  echo "Tagging docker image with tag ${git_tag}"
+#  sudo docker tag ${docker_image_id} sherlockpipe/sherlockpipe:latest
+#  sudo docker tag ${docker_image_id} sherlockpipe/sherlockpipe:${git_tag}
+#  echo "Push docker image with tag ${git_tag}"
+#  sudo docker push sherlockpipe/sherlockpipe:latest
+#  sudo docker push sherlockpipe/sherlockpipe:${git_tag}
+#  sudo docker images prune -all
+#  rm tests.log
 else
   echo "TESTS FAILED. See tests.log"
 fi
