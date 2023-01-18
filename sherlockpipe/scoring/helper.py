7420642ffcfa3f9b62e5ca2a909fe14c183cb269
martin.dvpj <martin.devora.pajares@gmail.com>
2023-01-18 21:06:47 +0000

    add helper methods

--- sherlockpipe/scoring/helper.py
+++ sherlockpipe/scoring/helper.py
@@ -23,3 +23,23 @@ def compute_border_score(time, result, intransit, cadence):
         transits_in_edge_count = len(transits_in_edge[transits_in_edge])
         border_score = 1 - transits_in_edge_count / len(transit_depths)
     return border_score if border_score >= 0 else 0
+
+def find_nearest(array, value):
+    idx = (np.abs(array - value)).argmin()
+    return idx, array[idx]
+
+def harmonic_spectrum(periods, spectrum):
+    harmonics = [1 / 4, 1 / 3, 1 / 2, 1, 2, 3, 4]
+    harmonic_spectrum = np.zeros(len(periods))
+    for index, period in enumerate(periods):
+        harmonic_sde = 0
+        non_nan_harmonics_count = 0
+        for harmonic in harmonics:
+            expected_harmonic_period = period * harmonic
+            harmonic_index, harmonic_period = find_nearest(periods, expected_harmonic_period)
+            harmonic_index = harmonic_index if np.abs(1 - harmonic_period / expected_harmonic_period) < 0.01 else np.nan
+            if not np.isnan(harmonic_index):
+                harmonic_sde = harmonic_sde + spectrum[harmonic_index]
+                non_nan_harmonics_count = non_nan_harmonics_count + 1
+        harmonic_spectrum[index] = harmonic_sde / non_nan_harmonics_count if non_nan_harmonics_count > 0 else 0
+    return harmonic_spectrum
