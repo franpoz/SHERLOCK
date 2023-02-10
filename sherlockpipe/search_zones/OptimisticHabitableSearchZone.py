93becf2e85a6f6d0ada4f71e9481c0099d38a6e4
martin.dvpj <martin.devora.pajares@gmail.com>
2023-02-10 14:16:44 +0000

    improve docs

--- sherlockpipe/search_zones/OptimisticHabitableSearchZone.py
+++ sherlockpipe/search_zones/OptimisticHabitableSearchZone.py
@@ -12,6 +12,13 @@ class OptimisticHabitableSearchZone(SearchZone):
         super().__init__()
 
     def calculate_period_range(self, star_info: StarInfo):
+        """
+        Calculates the minimum and maximum periods for the Optimistic Habitable Zone (from kopparapu et al 2013)
+        given the star_info.
+
+        :param star_info: the star where the range should be calculated
+        :return: a tuple of minimum_period and maximum_period
+        """
         habitability_calc = HabitabilityCalculator()
         hz_periods = habitability_calc.calculate_hz_periods(star_info.teff, star_info.lum, star_info.mass)
         if hz_periods is None:
