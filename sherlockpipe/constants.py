e3d89881df622f3e8862b643b6406b829b3e4147
martin.dvpj <martin.devora.pajares@gmail.com>
2023-02-12 11:21:57 +0000

    document constants

--- sherlockpipe/constants.py
+++ sherlockpipe/constants.py
@@ -1,6 +1,10 @@
 import os
 
+"""Constants used by SHERLOCK"""
+
 USER_HOME_ELEANOR_CACHE = os.path.join(os.path.expanduser('~'), '.eleanor/')
-USER_HOME_SHERLOCKPIPE = os.path.join(os.path.expanduser('~'), '.eleanor/')
+"""The directory where SHERLOCK will store the eleanor internal data"""
 AU_TO_RSUN = 215.032
+"""Constant used to convert Astronomical Units to Sun Radius"""
 MOMENTUM_DUMP_QUALITY_FLAG = 2 ** 5
+"""Quality flag value for momentum dumps"""
