867bf6d8a5fe08ffb96c73c7918c6b5fbba9ec0b
martin.dvpj <martin.devora.pajares@gmail.com>
2023-02-12 08:51:47 +0000

    add test resources

---
+++ sherlockpipe/tests/resources/RandomSignalSelector.py
@@ -0,0 +1,18 @@
+import random
+from sherlockpipe.scoring.SignalSelector import SignalSelector, SignalSelection
+
+
+class RandomSignalSelector(SignalSelector):
+    def __init__(self):
+        super().__init__()
+
+    def select(self, id_run, sherlock_target, star_info, transits_min_count, time, lcs, transit_results, wl, cadence):
+        best_signal_snr_index = random.randrange(0, len(transit_results) - 1, 1)
+        best_signal = transit_results[best_signal_snr_index]
+        best_signal_snr = best_signal.snr
+        if best_signal_snr > sherlock_target.snr_min:  # and SDE[a] > SDE_min and FAP[a] < FAP_max):
+            best_signal_score = 1
+        else:
+            best_signal_score = 0
+        # You could also extend SignalSelector class to provide more information about your choice.
+        return SignalSelection(best_signal_score, best_signal_snr_index, best_signal)
