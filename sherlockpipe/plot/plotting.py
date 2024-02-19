ba517b7b475ca872dce9747bc75be043b2930fee
martindevora <martin.devora.pajares@gmail.com>
2024-02-19 11:27:48 +0000

    add new plots

--- sherlockpipe/plot/plotting.py
+++ sherlockpipe/plot/plotting.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from lcbuilder.star.starinfo import StarInfo
+from matplotlib.colors import LinearSegmentedColormap
 from scipy import stats
-
+import pandas as pd
 from sherlockpipe.search.transitresult import TransitResult
 
 
@@ -96,3 +98,132 @@ def save_transit_plot(object_id: str, title: str, plot_dir: str, file: str, time
     plt.savefig(plot_dir + file, bbox_inches='tight', dpi=200)
     fig.clf()
     plt.close(fig)
+
+
+def plot_system_architecture(sistemas, temperaturas_estrellas, tamaños_estrellas, periodos_orbitales, tamaños_planetas, candidatos, save_dir) -> None:
+    sistemas_info = list(zip(sistemas, periodos_orbitales, tamaños_planetas, temperaturas_estrellas, tamaños_estrellas))
+    sistemas_info.sort(key=lambda x: x[3])
+    sistemas_ordenados, periodos_orbitales_ordenados, tamaños_planetas_ordenados, temperaturas_estrellas_ordenadas, tamaños_estrellas_ordenados = zip(*sistemas_info)
+    fig, ax = plt.subplots(figsize=(10, 4))
+    fig.patch.set_facecolor('xkcd:white')
+    separacion = 1.1
+    ylim_superior = len(sistemas_ordenados) * separacion + 0.5
+    def color_planeta(tamaño):
+        if tamaño < 2:
+            return 'brown'
+        elif 2 <= tamaño <= 5:
+            return 'green'
+        else:
+            return 'blue'
+    colors = [(1, 0, 0), (1, 1, 0)]  # R -> Y
+    n_bins = 100  # Número de divisiones en el colormap
+    cmap_name = 'my_list'
+    cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bins)
+    ax.set_ylim(-0.5, ylim_superior)
+    ax.set_yticks(np.arange(len(sistemas_ordenados)) * separacion)
+    ax.set_yticklabels(sistemas_ordenados)
+    ax.set_xlabel('Orbital Period (day)', fontsize=14)
+    # ax.set_ylabel('Planetary System')
+    ax.tick_params(axis='x', labelsize=12)  # Ajusta el tamaño de los ticks del eje X
+    ax.tick_params(axis='y', labelsize=12)  # Ajusta el tamaño de los ticks del eje Y
+    max_periodo_confirmados = max([max(periodos) for periodos in periodos_orbitales])
+    periodos_candidatos = [item for sublist in [c['periodos'] for c in candidatos.values()] for item in sublist]
+    max_periodo_candidatos = max(periodos_candidatos) if periodos_candidatos else 0
+    max_periodo_general = max(max_periodo_confirmados, max_periodo_candidatos)
+    ticks_x = np.arange(1, max_periodo_general + 1, 2)
+    ax.set_xticks(ticks_x)
+    # Ajustar los límites del eje X para asegurarse de que todos los ticks sean visibles
+    ax.set_xlim(-1.5, max_periodo_general + 1)
+    ax.grid(True, axis='y', linestyle='--', linewidth=0.5,zorder=1)
+    ax.grid(False, axis='x')
+    norm = plt.Normalize(3900, 6000)
+    for i, sistema in enumerate(sistemas_ordenados):
+        y = i * separacion  # Ajustar la separación entre sistemas
+        for j, periodo in enumerate(periodos_orbitales_ordenados[i]):
+            tamaño_planeta = tamaños_planetas_ordenados[i][j]
+            color = color_planeta(tamaño_planeta)
+            ax.scatter(periodo, y, s=tamaño_planeta*35, c=color, marker='o', alpha=1.0, zorder=2)  # Sin transparencia para confirmados
+        tamaño_estrella = tamaños_estrellas_ordenados[i] * 20 * 100  # Escala de tamaño de estrella\n",
+        #norm = plt.Normalize(3900, 6000)
+        color_estrella = cm(norm(temperaturas_estrellas_ordenadas[i]))
+        ax.scatter(-0.25, y, s=tamaño_estrella, c=color_estrella, marker='o', alpha=1.0,zorder=2)  # Sin transparencia para estrellas
+        # Planetas candidatos (rellenos con borde negro y transparencia)
+        if sistema in candidatos:
+            for k, periodo_candidato in enumerate(candidatos[sistema]['periodos']):
+                tamaño_candidato = candidatos[sistema]['radios'][k]
+                color_candidato = color_planeta(tamaño_candidato)
+                ax.scatter(periodo_candidato, y, s=tamaño_candidato*35, c=color_candidato, edgecolors='black', marker='o', alpha=0.5, linewidth=2,zorder=2)  # Añadir transparencia
+    # Crear colorbar para las temperaturas de las estrellas con el colormap personalizado
+    sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, ax=ax,pad=0.02)
+    cbar.set_label('Stellar Temperature (K)', fontsize=14)
+    legend_elements = [
+        plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='brown', markersize=10, label=r'≤ 2.0 R$_{\\oplus}$'),
+        plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='green', markersize=10, label=r'> 2.0 and ≤ 5.0 R$_{\\oplus}$'),
+        plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=10, label=r'> 5.0 R$_{\\oplus}$')
+    ]
+    ax.legend(handles=legend_elements, loc='upper center', bbox_to_anchor=(0.87, 0.98), ncol=1)
+    plt.tight_layout()
+    plt.savefig(save_dir, 'sistemas_planetarios.png', dpi=200)
+
+
+def plot_stability(masses, eccentricities, megnos, save_dir):
+    unique_masses = np.unique(masses)
+    unique_eccs = np.unique(eccentricities)
+    results = np.full((len(unique_masses), len(unique_eccs)), 10)
+    for index, mass in enumerate(masses):
+        mass_index = np.argwhere(unique_masses == mass).flatten()[0]
+        ecc = eccentricities[index]
+        ecc_index = np.argwhere(unique_eccs == ecc).flatten()[0]
+        results[mass_index, ecc_index] = megnos[index]
+    ecc_index = np.argwhere(unique_eccs == ecc).flatten()[0]
+    results = np.nan_to_num(results)
+    n = len(results)
+    pixels = len(results[1])
+    Ngrid = len(np.unique(masses))
+    ecc1max = max(eccentricities)
+    ecc1min = min(eccentricities)
+    par_m1 = np.linspace(ecc1min, ecc1max, Ngrid)
+    m2max = max(masses)
+    m2min = min(masses)
+    par_m2 = np.linspace(m2min, m2max, Ngrid)
+    ax = plt.subplot(111)
+    extent = [min(par_m2), max(par_m2), min(par_m1), max(par_m1)] #x,y
+    ax.set_xlim(extent[0], extent[1])
+    ax.set_xlabel("Mass $R_\\oplus$")
+    ax.set_ylim(extent[2], extent[3])
+    ax.set_ylabel("Eccentricity")
+    im = ax.imshow(np.transpose(results), interpolation="none", vmin=1., vmax=5, cmap="plasma", origin="lower", aspect='auto', extent=extent)
+    cb = plt.colorbar(im, ax=ax, ticks=[1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0])
+    cb.ax.set_yticklabels(["1.5", "2.0", "2.5", "3.0", "3.5", "4.0", "4.5", "5.0"])
+    cb.set_label("MEGNO $\\langle Y \\rangle$")
+    ecc = np.linspace(0, 0.356, 50)
+    ebb = 0.27 - (1.50*ecc)
+    plt.plot(ecc, ebb, '-', color="black")
+    plt.savefig(save_dir + '/stability_megno.png', bbox_inches='tight', dpi=200)
+
+#megno_df = pd.read_csv("/home/martin/Downloads/sherlock/sherlock_confusion_matrix/cps/done/TIC420779000_all/stability_stability/stability_megno.csv")
+#megno_df[['mass1', 'mass2']] = megno_df['masses'].str.split(',', expand=True)
+#megno_df['mass2'] = megno_df['mass2'].astype(float)
+#megno_df[['ecc1', 'ecc2']] = megno_df['eccentricities'].str.split(',', expand=True)
+#megno_df['ecc2'] = megno_df['ecc2'].astype(float)
+#megno_df['ecc1'] = megno_df['ecc1'].astype(float)
+#megno_df = megno_df.groupby(['mass2', 'ecc2'])['megno'].mean().reset_index()
+#megno_df.sort_values(by=['mass2', 'ecc2'], ascending=True, inplace=True)
+#plot_stability(megno_df['mass2'].to_numpy(), megno_df['ecc2'].to_numpy(), megno_df['megno'].to_numpy(),'./')
+
+# sistemas = ['TOI-2441', 'WASP-16', 'HAT-P-26', 'HAT-P-27']
+# periodos_orbitales = [[0.8], [3.2], [4.2], [3.03]]
+# tamaños_planetas = [[1.7], [11.8], [7.1], [11]]
+# temperaturas_estrellas = [4099, 5717, 5062, 5316]
+# tamaños_estrellas = [0.72, 1.07, 0.86, 0.86]
+# candidatos = {
+#     'TOI-2441': {'radios': [3.0], 'periodos': [18.7]},
+#     'WASP-16': {'radios': [2.0], 'periodos': [10.4]},
+#     'HAT-P-26': {'radios': [2.1], 'periodos': [6.6]},
+#     'HAT-P-27': {'radios': [3.03], 'periodos': [1.2]}
+# }
+# plot_system_architecture(sistemas, temperaturas_estrellas, tamaños_estrellas, periodos_orbitales, tamaños_planetas, candidatos, './')
+
+
