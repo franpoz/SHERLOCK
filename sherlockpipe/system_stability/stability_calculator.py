a269129c752f3ff7e8c9130765e37fb6e33898bf
martindevora <martin.devora.pajares@gmail.com>
2024-05-13 10:17:52 +0100

    improve megno with more scenarios

--- sherlockpipe/system_stability/stability_calculator.py
+++ sherlockpipe/system_stability/stability_calculator.py
@@ -24,8 +24,9 @@ class PlanetInput:
     def __init__(self, period, period_low_err, period_up_err,
                  radius, radius_low_err, radius_up_err, eccentricity, ecc_low_err, ecc_up_err,
                  inclination, inc_low_err, inc_up_err, omega, omega_low_err, omega_up_err,
+                 omega_big=None, omega_big_low_err=None, omega_big_up_err=None,
                  mass=None, mass_low_err=None, mass_up_err=None, period_bins=3,
-                 mass_bins=3, ecc_bins=3, inc_bins=3, omega_bins=3):
+                 mass_bins=3, ecc_bins=3, inc_bins=3, omega_bins=3, omega_big_bins=3):
         self.period = period
         self.period_low_err = period_low_err
         self.period_up_err = period_up_err
@@ -38,6 +39,9 @@ class PlanetInput:
         self.inclination = inclination
         self.inc_low_err = inc_low_err
         self.inc_up_err = inc_up_err
+        self.omega_big = omega_big
+        self.omega_big_low_err = omega_big_low_err
+        self.omega_big_up_err = omega_big_up_err
         self.omega = omega
         self.omega_low_err = omega_low_err
         self.omega_up_err = omega_up_err
@@ -49,6 +53,7 @@ class PlanetInput:
         self.ecc_bins = ecc_bins if ecc_bins is not None else 3
         self.inc_bins = inc_bins if inc_bins is not None else 3
         self.omega_bins = omega_bins if omega_bins is not None else 3
+        self.omega_big_bins = omega_big_bins if omega_big_bins is not None else 3
 
 
 class SimulationInput:
@@ -56,13 +61,14 @@ class SimulationInput:
     Used as input for the simulations done for each scenario
     """
 
-    def __init__(self, star_mass, mass_arr, planet_periods, ecc_arr, inc_arr, omega_arr, index):
+    def __init__(self, star_mass, mass_arr, planet_periods, ecc_arr, inc_arr, omega_arr, omega_big_arr, index):
         self.star_mass = star_mass
         self.mass_arr = mass_arr
         self.planet_periods = planet_periods
         self.ecc_arr = ecc_arr
         self.inc_arr = inc_arr
         self.omega_arr = omega_arr
+        self.omega_big_arr = omega_big_arr
         self.index = index
 
 
@@ -148,18 +154,20 @@ class StabilityCalculator(ABC):
         sim.integrator = "whfast"
         sim.ri_whfast.safe_mode = 0
         sim.add(m=simulation_input.star_mass)
-        min_period = 100
+        min_period = 1000
         for planet_key, mass in enumerate(simulation_input.mass_arr):
             period = simulation_input.planet_periods[planet_key]
             min_period = min_period if period > min_period else period
             ecc = simulation_input.ecc_arr[planet_key]
             inc = np.deg2rad(simulation_input.inc_arr[planet_key])
             if simulation_input.omega_arr[planet_key] == 'rand':
-                omega = random.uniform(0, 360)
-            else:
-                omega = np.deg2rad(simulation_input.omega_arr[planet_key])
+                simulation_input.omega_arr[planet_key] = random.uniform(0, 360)
+            omega = np.deg2rad(simulation_input.omega_arr[planet_key])
+            if simulation_input.omega_big_arr[planet_key] == 'rand':
+                simulation_input.omega_big_arr[planet_key] = random.uniform(0, 360)
+            omega_big = np.deg2rad(simulation_input.omega_big_arr[planet_key])
             sim.add(m=LcbuilderHelper.convert_from_to(mass, u.M_earth, u.M_sun) / simulation_input.star_mass,
-                    P=LcbuilderHelper.convert_from_to(period, u.day, u.year), e=ecc, omega=omega, inc=inc)
+                    P=LcbuilderHelper.convert_from_to(period, u.day, u.year), e=ecc, omega=omega, Omega=omega_big, inc=inc)
         sim.dt = LcbuilderHelper.convert_from_to(min_period, u.day, u.year) * self.dt
         # sim.status()
         sim.move_to_com()
@@ -188,6 +196,7 @@ class StabilityCalculator(ABC):
         planet_ecc = []
         planet_inc = []
         planet_omega = []
+        planet_omega_big = []
         for planet_param in planet_params:
             if planet_param.period_bins == 1 or planet_param.period_low_err == planet_param.period_up_err == 0:
                 period_grid = np.full(1, planet_param.period)
@@ -231,11 +240,22 @@ class StabilityCalculator(ABC):
                                          planet_param.omega + planet_param.omega_up_err,
                                          planet_param.omega_bins)
             planet_omega.append(omega_grid)
+            if "omega" in free_params:
+                # using arange instead of linspace because 0 and 360 are the same, so we exclude 360
+                omega_big_grid = np.arange(0, 360, 360 // planet_param.omega_big_bins)
+            elif planet_param.omega_big_bins == 1 or planet_param.omega_big_low_err == planet_param.omega_big_up_err == 0:
+                omega_grid = np.full(1, planet_param.omega_big)
+            else:
+                omega_grid = np.linspace(planet_param.omega_big - planet_param.omega_big_low_err,
+                                         planet_param.omega_big + planet_param.omega_big_up_err,
+                                         planet_param.omega_big_bins)
+            planet_omega_big.append(omega_grid)
         period_grid = np.array(np.meshgrid(*np.array(planet_period, dtype=object))).T.reshape(-1, len(planet_period))
         masses_grid = np.array(np.meshgrid(*np.array(planet_masses, dtype=object))).T.reshape(-1, len(planet_masses))
         ecc_grid = np.array(np.meshgrid(*np.array(planet_ecc, dtype=object))).T.reshape(-1, len(planet_ecc))
         inc_grid = np.array(np.meshgrid(*np.array(planet_inc, dtype=object))).T.reshape(-1, len(planet_inc))
         omega_grid = np.array(np.meshgrid(*np.array(planet_omega, dtype=object))).T.reshape(-1, len(planet_omega))
+        omega_big_grid = np.array(np.meshgrid(*np.array(planet_omega, dtype=object))).T.reshape(-1, len(planet_omega))
         simulation_inputs = []
         i = 0
         star_masses_scenario_num = len(star_masses)
@@ -244,8 +264,9 @@ class StabilityCalculator(ABC):
         inc_scenario_num = len(inc_grid)
         ecc_scenario_num = len(ecc_grid)
         omega_scenario_num = len(omega_grid)
+        omega_big_scenario_num = len(omega_big_grid)
         scenarios_num = star_masses_scenario_num * masses_scenario_num * period_scenario_num * inc_scenario_num * \
-                        ecc_scenario_num * omega_scenario_num
+                        ecc_scenario_num * omega_scenario_num * omega_big_scenario_num
         logging.info("Preparing system values for all scenarios")
         logging.info("%.0f star mass scenarios.", len(star_masses))
         logging.info("%.0f bodies mass scenarios.", len(masses_grid))
@@ -253,8 +274,9 @@ class StabilityCalculator(ABC):
         logging.info("%.0f inclination scenarios.", len(inc_grid))
         logging.info("%.0f eccentricity scenarios.", len(ecc_grid))
         logging.info("%.0f arg of periastron scenarios.", len(omega_grid))
-        logging.info("%.0f x %.0f x %.0f x %.0f x %.0f x %.0f = %.0f total scenarios.", star_masses_scenario_num,
-                     masses_scenario_num, period_scenario_num, inc_scenario_num, ecc_scenario_num, omega_scenario_num,
+        logging.info("%.0f long of asc node scenarios.", len(omega_big_grid))
+        logging.info("%.0f x %.0f x %.0f x %.0f x %.0f x %.0f x %.0f = %.0f total scenarios.", star_masses_scenario_num,
+                     masses_scenario_num, period_scenario_num, inc_scenario_num, ecc_scenario_num, omega_scenario_num, omega_big_scenario_num,
                      scenarios_num)
         for star_mass in star_masses:
             for period_key, period_arr in enumerate(period_grid):
@@ -262,7 +284,8 @@ class StabilityCalculator(ABC):
                     for inc_key, inc_arr in enumerate(inc_grid):
                         for ecc_key, ecc_arr in enumerate(ecc_grid):
                             for omega_key, omega_arr in enumerate(omega_grid):
-                                simulation_inputs.append(SimulationInput(star_mass, mass_arr, period_arr, ecc_arr, inc_arr, omega_arr, i))
+                                for omega_big_key, omega_big_arr in enumerate(omega_big_grid):
+                                    simulation_inputs.append(SimulationInput(star_mass, mass_arr, period_arr, ecc_arr, inc_arr, omega_arr, omega_big_arr, i))
                                 i = i + 1
         logging.info("Finished preparing scenarios")
         with Pool(processes=cpus) as pool:
