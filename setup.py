3e383315b5365b8eb81e8c98d36b9b9719c7a657
martindevora <martin.devora.pajares@gmail.com>
2025-07-15 14:11:07 +0100

    Preparing release 1.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command.install import install
 import platform
 import os
 import shutil
-version = "1.0.6"
+version = "1.0.7"
 import subprocess
 
 with open("README.md", "r") as fh:
