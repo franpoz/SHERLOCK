0b21b68254e3a9b01c03f1cdfa961b700f0d952b
martindevora <martin.devora.pajares@gmail.com>
2025-07-15 10:33:10 +0100

    Preparing release 1.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command.build_py import build_py
 import platform
 import os
 import shutil
-version = "1.0.5"
+version = "1.0.6"
 import subprocess
 
 with open("README.md", "r") as fh:
