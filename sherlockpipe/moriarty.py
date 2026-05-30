578a36b8861507854100256c952fa571ac65a723
martindevora <martin.devora.pajares@gmail.com>
2026-05-30 21:45:57 +0100

    fix moriarty mask candidates bug

--- sherlockpipe/moriarty.py
+++ sherlockpipe/moriarty.py
@@ -7,8 +7,11 @@ if __name__ == '__main__':
     ap = ArgumentParser(description='Vetting of Sherlock objects of interest')
     ap.add_argument('--object_dir', help="If the object directory is not your current one you need to provide the "
                                          "ABSOLUTE path", required=False)
-    ap.add_argument('--mask_candidate', type=str, default=[], help="The candidate signal to be used.", required=False)
+    ap.add_argument('--mask_candidate', type=str, default='', help="The candidate signal to be used.", required=False)
     ap.add_argument('--batch_size', type=int, default=256, help="The candidate signal to be used.", required=False)
     ap.add_argument('--threshold', type=float, default=0.5, help="The candidate signal to be used.", required=False)
     args = ap.parse_args()
-    run_moriarty(args.object_dir, [int(candidate) - 1 for candidate in args.mask_candidate.split(',')], args.batch_size, args.threshold)
+    run_moriarty(args.object_dir,
+                 [int(candidate) - 1 for candidate in args.mask_candidate.split(',')] if args.mask_candidate is not '' else [],
+                 args.batch_size,
+                 args.threshold)
